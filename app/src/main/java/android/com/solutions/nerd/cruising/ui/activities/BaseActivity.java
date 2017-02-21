8332a5a9c72b0e05dd0727964322559fd130a7ca
mookiejones <charles.heath.berman@gmail.com>
2017-02-21 06:10:31 -0500

    hopefullly fixed

--- app/src/main/java/android/com/solutions/nerd/cruising/ui/activities/BaseActivity.java
+++ app/src/main/java/android/com/solutions/nerd/cruising/ui/activities/BaseActivity.java
@@ -1,5 +1,6 @@
 package android.com.solutions.nerd.cruising.ui.activities;
 
+import android.com.solutions.nerd.cruising.BuildConfig;
 import android.com.solutions.nerd.cruising.interfaces.IVehicle;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,8 +11,6 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ui.ActivityHelper;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Arrays;
-
 /**
  * Created by mookie on 1/28/17.
  * for Nerd.Solutions
@@ -19,11 +18,15 @@ import java.util.Arrays;
 
 public abstract class BaseActivity  extends AppCompatActivity {
     private static final String TAG=BaseActivity.class.getSimpleName();
-
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
+    protected ActivityHelper mActivityHelper;
     private IVehicle vehicle;
-    public IVehicle getVehicle(){return vehicle;}
-    public void setVehicle(IVehicle vehicle){this.vehicle=vehicle;}
     private boolean destroyed;
+
+    public IVehicle getVehicle(){return vehicle;}
+
+    public void setVehicle(IVehicle vehicle){this.vehicle=vehicle;}
 
     protected abstract int getLayoutResId();
 
@@ -31,14 +34,10 @@ public abstract class BaseActivity  extends AppCompatActivity {
         return destroyed;
     }
 
-    // Choose an arbitrary request code value
-    private static final int RC_SIGN_IN = 123;
-
-
-    protected ActivityHelper mActivityHelper;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
 
         // Check firebase
@@ -48,24 +47,30 @@ public abstract class BaseActivity  extends AppCompatActivity {
         }else{
             Log.d(TAG,"creating sign in");
             SignIn();
-//            startActivityForResult(
-//                    // Get an instance of AuthUI based on top of app
-//                    AuthUI.getInstance().createSignInIntentBuilder().build(),
-//                    RC_SIGN_IN
-//            );
-
-            // Not signed in
         }
     }
 
     private void SignIn(){
+
+
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
-                                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
-                                new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+//                        .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()
+//                                ,new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+//                                ,new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()
+//                                ,new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()
+//
+                        //                      ))
+                        /*
+                        By default, FirebaseUI uses Smart Lock for Passwords to store the user's credentials
+                        and automatically sign users into your app on subsequent attempts. Using Smart Lock is
+                        recommended to provide the best user experience, but in some cases you may want to disable
+                        Smart Lock for testing or development. To disable Smart Lock, you can use the setIsSmartLockEnabled
+                        method when building your sign-in Intent:
+                         */
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+//                        .setTheme(R.style.Sup)
                         .build(),
                 RC_SIGN_IN);
     }
---
+++ app/src/main/java/android/com/solutions/nerd/cruising/ui/activities/ChooserActivity.java
@@ -0,0 +1,9 @@
+package android.com.solutions.nerd.cruising.ui.activities;
+
+/**
+ * Created by mookie on 2/21/17.
+ * for Nerd.Solutions
+ */
+
+public class ChooserActivity {
+}
